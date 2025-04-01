4da9067f1e4cb87c51a937316da09416c12ec0ae
Lucaasshq <lucashenriquebarros2016@gmail.com>
2025-04-01 17:52:41 -0300

    feature:()adicionando sidebar

--- myMoneys/src/app/nav-bar/nav-bar.component.ts
+++ myMoneys/src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [SidebarModule, ButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  exibirMenu:boolean = false
 
 }
